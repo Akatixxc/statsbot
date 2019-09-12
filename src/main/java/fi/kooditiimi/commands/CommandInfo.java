f6ea2e8b26c90cd9b4f910f28bf53f66eba0a652
BuildTools <unconfigured@null.spigotmc.org>
2019-09-12 13:13:12 +0300

    Command Classit statbotille

---
+++ src/main/java/fi/kooditiimi/commands/CommandInfo.java
@@ -0,0 +1,22 @@
+package fi.kooditiimi.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+
+
+public class CommandInfo {
+
+    public static void commandInfo(GuildMessageReceivedEvent event) {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("Statsbot - Info");
+        embed.setDescription("LAMAO");
+        embed.addField("Version number", "No", true);
+        embed.addField("Info", "Literally useless", true);
+        embed.setColor(Color.RED);
+        event.getChannel().sendMessage(embed.build()).queue();
+        embed.clear();
+    }
+
+}
